a6a2ee4a089c4e08c44927f0ef9200a37dbd9382
Damirkhan <d.aloev@student.inha.uz>
2021-12-16 21:17:42 +0500

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,94 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
+import Home from '../views/Home.vue'
+import Product from '../views/Product.vue'
+import Category from '../views/Category.vue'
+import Search from '../views/Search.vue'
+import Cart from '../views/Cart.vue'
+import SignUp from '../views/SignUp.vue'
+import Login from '../views/Login.vue'
+import MyAccount from '../views/MyAccount.vue'
+import Checkout from '../views/Checkout.vue'
+import Success from '../views/Success.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/:category_slug/:product_slug', 
+    name: 'Product',
+    component: Product
+  },
+  {
+    path: '/:category_slug', 
+    name: 'Category',
+    component: Category
+  },
+  {
+    path: '/search', 
+    name: 'Search',
+    component: Search
+  },
+  {
+    path: '/cart', 
+    name: 'Cart',
+    component: Cart 
+  },
+  {
+    path: '/sign-up', 
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/login', 
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/myAccount', 
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/cart/checkout', 
+    name: 'Checkout',
+    component: Checkout,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/cart/success', 
+    name: 'Success',
+    component: Success,
+    meta: {
+      requireLogin: true
+    }
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'Login', query: {to: to.path} });
+  } else {
+    next()
+  }
+})
+
+export default router
